6c5d878d2bf741ab19455c2a441420ab7a6fb1d6
Kouros <kamousavi@gmail.com>
2020-05-28 12:01:13 -0400

    completed routes. added inventory functionality. added orders page

--- server.js
+++ server.js
@@ -22,34 +22,66 @@ app.get("/product/:id", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/product.html"));
 });
 
+app.get("/orders", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/orders.html"));
+});
+
 app.get("/api/getProduct/:id", function (req, res) {
     db.Product.find({ _id: req.params.id }).then(results => {
         res.json(results);
     });
 });
 
-app.post("/api/form/", function (req, res) {
-    db.Customer.create({
-        fname: req.body.name,
-        email: req.body.email
-    }).then(customer => {
-        db.Order.create({
-            customer: customer._id,
-            product: req.body.productId,
-            qty: req.body.quantity
-        }).then(order => {
-            console.log("order._id is: " + order._id);
-            console.log("customer._id is: " + customer._id);
-            db.Customer.findOneAndUpdate(
-                {_id: customer._id},
-                {
-                    $push: {orders: order._id}
-                }
-                //{new: true}
-            );
-        });
-    })
-})
+app.get("/api/orders", function(req, res) {
+    db.Order.find({}).populate("customer").populate("product").then(results => res.json(results));
+});
+
+app.post("/api/form/", ({ body }, res) => {
+    // The data being submitted is for both the Order and Customer models, so I'm splitting it up here
+    const orderData = { qty: body.quantity, product: body.productId }
+    const customerData = { fname: body.name, lname: body.name, email: body.email }
+    // The first step is to find the customer if he/she already exists, or to create a new one
+    // A little research found the "upsert" option for findOneAndUpdate
+    db.Customer.findOneAndUpdate(
+        { email: body.email },
+        customerData,
+        { new: true, upsert: true }
+        // One the customer is found/created, we can create the order, and put the customer _id into it
+    ).then(function (newCustomerData) {
+        orderData.customer = newCustomerData._id   // modifying the orderData object to include customer's _id
+        db.Order.create(orderData)
+            // Once the new order is created, we now UPDATE the new customer record with the order _id value
+            // Note that my find query on the e-mail value; I think this is needed because the findAndUpdate() method
+            // here does not have access to the original Customer first written above
+            .then(function (newOrder) {
+                db.Customer.findOneAndUpdate(
+                    { email: body.email },
+                    { $push: { orders: newOrder._id } },
+                    { new: true }
+                ).then(resp => {
+                    if (resp && resp._id !== undefined) {
+                        //res.json({ result: "success" })
+                        res.sendFile(path.join(__dirname, "./public/index.html"));
+                    } else {
+                        res.status(400).json({ result: "fail" })
+                    }
+                })
+            })
+            .catch(err => {
+                res.json(err)
+            });
+    });
+    db.Product.findOne({ _id: body.productId }).then(product => {
+        console.log("product inventory: " + product.inventory);
+        if ((product.inventory - body.quantity) > 0) {
+            db.Product.findOneAndUpdate({ _id: body.productId },
+                { inventory: product.inventory - body.quantity }
+            ).then(result => console.log(result));
+        } else {
+            res.send("Not enough inventory.");
+        }
+    });
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/storedb", { useNewUrlParser: true });
 
